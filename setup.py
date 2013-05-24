25ea92aa85290c20be4930bd95c431eda8dc3ef7
James Brown <jbrown@uber.com>
2013-05-23 17:55:45 -0700

    typo

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
             'haup = hacheck.haupdown:up',
             'hadown = hacheck.haupdown:down',
             'hastatus = hacheck.haupdown:status',
-            'hacheck = hacheck.main.main',
+            'hacheck = hacheck.main:main',
         ]
     },
     classifiers=[
