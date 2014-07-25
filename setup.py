c9992f0a7e1fbe0ec6f4efaf797cbdf2aadeeda6
James Brown <jbrown@uber.com>
2014-07-24 19:21:45 -0700

    bump to version 0.5.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ install_requires, dependency_links = get_install_requirements()
 
 setup(
     name="hacheck",
-    version="0.5.0",
+    version="0.5.1",
     author="James Brown",
     author_email="jbrown@uber.com",
     url="https://github.com/uber/hacheck",
