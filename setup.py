a7e65eda50418a3b788643d97efb7c056ff41791
James Brown <jbrown@uber.com>
2015-01-21 12:07:07 -0800

    add Python 3.4 to trove classifiers

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: MIT License",
