f347a01bce5ecbfcc14f0742917046a23935c7c5
James Brown <jbrown@uber.com>
2015-04-21 12:56:02 -0700

    fix tests

--- tests/test_callables.py
+++ tests/test_callables.py
@@ -19,27 +19,27 @@ class TestCallable(TestCase):
         with nested(
                 mock.patch.object(hacheck, 'spool', return_value=(True, {})),
                 mock.patch.object(hacheck.haupdown, 'print_s'),
-                mock.patch('sys.argv', ['ignored', sentinel_service_name] + list(args))
+                mock.patch('sys.argv', ['ignored'] + list(args))
         ) as (mock_spool, mock_print, _1):
             yield mock_spool, mock_print
 
     def test_basic(self):
         with self.setup_wrapper() as (spooler, _):
             spooler.status.return_value = (True, {})
-            hacheck.haupdown.main()
+            hacheck.haupdown.main('status_downed')
             spooler.configure.assert_called_once_with('/var/spool/hacheck', needs_write=False)
 
     def test_exit_codes(self):
-        with self.setup_wrapper() as (spooler, mock_print):
+        with self.setup_wrapper([sentinel_service_name]) as (spooler, mock_print):
             spooler.status.return_value = (True, {})
-            self.assertEqual(0, hacheck.haupdown.main())
+            self.assertEqual(0, hacheck.haupdown.main('status'))
             mock_print.assert_any_call('UP\t%s', sentinel_service_name)
             spooler.status.return_value = (False, {'reason': 'irrelevant'})
-            self.assertEqual(1, hacheck.haupdown.main())
+            self.assertEqual(1, hacheck.haupdown.main('status'))
             mock_print.assert_any_call('DOWN\t%s\t%s', sentinel_service_name, 'irrelevant')
 
     def test_up(self):
-        with self.setup_wrapper() as (spooler, mock_print):
+        with self.setup_wrapper([sentinel_service_name]) as (spooler, mock_print):
             hacheck.haupdown.up()
             spooler.up.assert_called_once_with(sentinel_service_name)
             self.assertEqual(mock_print.call_count, 0)
@@ -47,20 +47,20 @@ class TestCallable(TestCase):
     def test_down(self):
         os.environ['SSH_USER'] = 'testyuser'
         os.environ['SUDO_USER'] = 'testyuser'
-        with self.setup_wrapper() as (spooler, mock_print):
+        with self.setup_wrapper([sentinel_service_name]) as (spooler, mock_print):
             hacheck.haupdown.down()
             spooler.down.assert_called_once_with(sentinel_service_name,
                                                  'testyuser')
             self.assertEqual(mock_print.call_count, 0)
 
     def test_down_with_reason(self):
-        with self.setup_wrapper(['-r', 'something']) as (spooler, mock_print):
+        with self.setup_wrapper(['-r', 'something', sentinel_service_name]) as (spooler, mock_print):
             hacheck.haupdown.down()
             spooler.down.assert_called_once_with(sentinel_service_name, 'something')
             self.assertEqual(mock_print.call_count, 0)
 
     def test_status(self):
-        with self.setup_wrapper() as (spooler, mock_print):
+        with self.setup_wrapper([sentinel_service_name]) as (spooler, mock_print):
             spooler.status.return_value = (True, {})
             hacheck.haupdown.status()
             spooler.status.assert_called_once_with(sentinel_service_name)
